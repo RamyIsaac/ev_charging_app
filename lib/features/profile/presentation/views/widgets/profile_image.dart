125b000c725197420ee128fea5f959f050b643c1
Ramy Isaac <ramyeshak191@gmail.com>
2025-04-17 23:46:49 +0200

    add firestore to project

--- lib/features/profile/presentation/views/widgets/profile_image.dart
+++ lib/features/profile/presentation/views/widgets/profile_image.dart
@@ -9,8 +9,8 @@ class ProfileImage extends StatelessWidget {
   Widget build(BuildContext context) {
     return const ClipRRect(
       child: CircleAvatar(
-        backgroundImage: AssetImage('assets/images/Designer (1).jpeg'),
-        radius: 40,
+        backgroundImage: AssetImage('assets/images/on_boarding.png'),
+        radius: 70,
       ),
     );
   }
